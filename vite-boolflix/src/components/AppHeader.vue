88e97c54684e2a9f3acef8230eab0c96c55eb7cd
Limson91 <matteo.a.colautti@gmail.com>
2023-11-10 16:50:46 +0100

    add store and header component

---
+++ vite-boolflix/src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script>
+import { store } from '../store'
+
+export default {
+    data() {
+        return {
+            store: store,
+        }
+    },
+
+    methods: {
+        searchCall() {
+            this.$emit('doSearch')
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="searchbar">
+            <input type="text" v-model="store.searchString">
+            <button @click="searchCall">Search!</button>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.searchbar {
+    display: flex;
+}
+</style>
