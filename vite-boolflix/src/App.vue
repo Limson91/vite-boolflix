4a4a42f1b55c6f3188a99ac6a909cbc0d4d1cc69
Limson91 <matteo.a.colautti@gmail.com>
2023-11-10 17:20:58 +0100

    add header template in App.vue

--- vite-boolflix/src/App.vue
+++ vite-boolflix/src/App.vue
@@ -2,15 +2,15 @@
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import { store } from './store';
+import AppHeader from './components/AppHeader.vue';
 
 export default {
   data() {
     return {
       api_key: '51194c244fcf78d6ef455b6b6578df95',
       query: 'Ritorno al futuro'
-    }
+    };
   },
-
   methods: {
     fetchMovies() {
       axios.get('https://api.themoviedb.org/3/search/movie', {
@@ -20,41 +20,18 @@ export default {
         }
       }).then(res => {
         console.log(res.data.results);
-      })
+      });
     }
   },
-
   created() {
-    this.fetchMovies()
-  }
+    this.fetchMovies();
+  },
+  components: { AppHeader }
 }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader @doSearch="searchTitle" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
